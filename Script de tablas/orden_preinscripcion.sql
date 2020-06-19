e947e99e50bed276ce39cf9ac8dff0d274a4d4ed
franj <HZ8FZWJf>
2020-06-19 19:40:35 +0200

    actualizacion fichero

--- Script de tablas/orden_preinscripcion.sql
+++
@@ -1,4 +0,0 @@
-select distinct orden_preinscripcion from dv_uex_190401_filtrado_personas;
-select distinct tipo_studio from dv_uex_190401_filtrado_personas;
-select count (*) from dv_uex_190401_filtrado_personas where  tipo_studio like '%Grado%';
-select count (*) from dv_uex_190401_filtrado_personas where orden_preinscripcion like '%No disponible%' and tipo_studio like '%Grado%';
--- Script de tablas/script para tabla estudioPadres.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT * from "NivelEstudioPadres_2017_18";
-
-delete from "NivelEstudioPadres_2017_18" where curso LIKE '%curso%';
-alter table "NivelEstudioPadres_2017_18" alter column cod_plan type integer using cod_plan::integer;
-alter table "NivelEstudioPadres_2017_18" alter column cod_centro type integer using cod_centro::integer;
-delete from "NivelEstudioPadres_2017_18" where dni not in (select distinct dni from dv_uex_190401_filtrado_personas)
